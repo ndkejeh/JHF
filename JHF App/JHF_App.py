7b1658620767e9e43d0a276c577ebebac3a1039a
ndkejeh <nkejeh@decharles.com>
2017-04-10 12:07:37 +0100

    Add files via upload

---
+++ JHF App/JHF_App.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, url_for, jsonify, abort
+from UserDbConfig import db
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return  render_template("index.html")
+
+@app.route("/adduser")
+def adduser():
+    return render_template("AddUserForm.html")
+
+@app.route("/jhf/api/v1.0/users", methods=["GET", "POST"]) #this will be the api for add user
+def submitted():
+    if request.method == 'POST':
+        if not request.json:
+            abort(400) #data sent wasn't JSON so about with an error else process
+
+        new_entry = prospects(request.json["fname"], request.json["lname"], request.json["dob"], request.json["retirement_age"])
+        db.session.add(new_entry)
+        db.session.commit()
+        return jsonify( {"New Prospect" : new_entry} ),201 #201 is HTTP code for created
+    else:
+        return "It's likely a GET submission!"
+    #You then need the request method to access the data
+    #request.form['firstname']
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ JHF App/UserDbConfig.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+#db = sqlite3.connect('clients.db') #creates a new db if it doesn't exist
+#db.close()
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:jkl64fds@localhost:3300/jhf_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db  = SQLAlchemy(app)
+
+class prospects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(45))
+    lname = db.Column(db.String(45))
+    dob = db.Column(db.Date)
+    retirement_age = db.Column(db.Integer)
+    referrer_id = db.Column(db.Integer, db.ForeignKey('referrers.id'))
+
+    def __init__(self, fname, lname,dob,retirement_age):
+        self.fname = fname
+        self.lname = lname
+        self.dob = datetime.datetime.strptime(dob, "%d%m%Y")
+        self.retirement_age = retirement_age
+
+class referrers(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(45))
+    industry = db.Column(db.String(100))
+    prospects = db.relationship('prospects', backref='referrers', lazy='dynamic')
+
+db.create_all()
